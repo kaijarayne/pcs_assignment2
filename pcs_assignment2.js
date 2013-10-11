fa8ec27b51964cf9d9bc14c87a171c69592f6cce
kaijarayne <kaijarayne@gmail.com>
2013-10-10 18:19:44 -0700

    second assignment

---
+++ pcs_assignment2.js
@@ -0,0 +1,55 @@
+var myName = "Kaija Rayne";
+
+var output = "";
+
+for (var i = 10; i >= 0 ; i -= 1) {
+	switch (myName[i]) {
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "u":
+		break;
+		default: 
+
+		output = output + myName[i]
+
+	}
+}
+
+console.log(output);
+
+
+var candyTime = "Three Musketeers for the win!";
+
+var output = "";
+
+for (var i = 28; i >= 0 ; i -= 1) {
+	switch (candyTime[i]) {
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "u":
+		break;
+		default:
+
+		output = output + candyTime[i]
+
+	}
+}
+
+console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
